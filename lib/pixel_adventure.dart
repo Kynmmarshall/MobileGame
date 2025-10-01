44a4b4cd2a40e3ae72fe4dd83d0ac9b50e401f60
Kynmmarshall <kynmmarshall@gmail.com>
2025-10-01 23:54:46 +0100

    update

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
 
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-import 'package:pixel_adventure/levels/level.dart';
+import 'package:game/levels/level.dart';
 
 class PixelAdventure extends FlameGame{
+  late final CameraComponent cam;
 
+  final world = Level();
   @override
   FutureOr<void> onLoad() {
-    add(Level());
+
+    cam= CameraComponent.withFixedResolution(world: world, width: 640, height: 360);cam.viewfinder.anchor= Anchor.topLeft;
+     addAll([cam,world]);  
     return super.onLoad();
-  }
-}  
+  } 
+}     
