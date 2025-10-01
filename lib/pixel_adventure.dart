db9b8a3eedbe78b9cf9c0455605e3dda2c21a562
Kynmmarshall <kynmmarshall@gmail.com>
2025-10-01 23:32:30 +0100

    update

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,6 +1,13 @@
-import 'package:flame/flame.dart';
+import 'dart:async';
+
 import 'package:flame/game.dart';
+import 'package:pixel_adventure/levels/level.dart';
 
 class PixelAdventure extends FlameGame{
-  
-}
+
+  @override
+  FutureOr<void> onLoad() {
+    add(Level());
+    return super.onLoad();
+  }
+}  
