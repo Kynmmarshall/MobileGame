da31db8ca5126a3f25b7114fbbd8b8f303c022bc
Kynmmarshall <kynmmarshall@gmail.com>
2025-10-03 10:17:23 +0100

    update

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -11,12 +11,16 @@ class PixelAdventure extends FlameGame{
   late final CameraComponent cam;
 
   final world = Level();
-  @override
-  FutureOr<void> onLoad() {
 
+  @override
+  FutureOr<void> onLoad() async{
+    
+    //loac all images into the cache
+    await images.loadAllImages();
     cam= CameraComponent.withFixedResolution(world: world, width: 640, height: 360);
     cam.viewfinder.anchor= Anchor.topLeft;
-     addAll([cam,world]);  
+    addAll([cam,world]);  
+
     return super.onLoad();
   } 
 }     
