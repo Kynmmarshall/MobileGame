425939d768c6daee19aaa82ed54fb6de45a4e21f
Kynmmarshall <kynmmarshall@gmail.com>
2025-10-05 16:52:15 +0100

    update

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -6,8 +6,6 @@ import 'package:game/pixel_adventure.dart';
 
 enum PlayerState {idle, running}
 
-enum PlayerDirection {left, right, none}
-
 class Player extends SpriteAnimationGroupComponent with HasGameReference<PixelAdventure>, KeyboardHandler{
 
 String character;
@@ -16,10 +14,9 @@ Player({ position, this.character = 'Ninja Frog'}): super(position: position);
 late final SpriteAnimation idleAnimation,runningAnimation;
 final double stepTime=0.05;
 
-PlayerDirection playerDirection = PlayerDirection.none;
+
 double moveSpeed = 100;
 Vector2 velocity = Vector2(0, 0);
-bool isfacingright = true;
 
 // Loads Assets to the game (inbuilt function that can be modified)
   @override
@@ -42,16 +39,7 @@ bool isfacingright = true;
     final isRightKeyPressed = keysPressed.contains(LogicalKeyboardKey.arrowRight) ||
           keysPressed.contains(LogicalKeyboardKey.keyD);
     
-    if(isRightKeyPressed && isLeftKeyPressed){
-      playerDirection = PlayerDirection.none;
-    }else if (isRightKeyPressed){
-      playerDirection = PlayerDirection.right;
-    }else if (isLeftKeyPressed){
-      playerDirection = PlayerDirection.left;
-    }
-    else{
-      playerDirection = PlayerDirection.none;
-    }
+    
     return super.onKeyEvent(event, keysPressed);
   }
 
@@ -80,30 +68,8 @@ bool isfacingright = true;
  
   void _updatePosition(double dt) {
 
-    double dirX = 0.0;
-    switch (playerDirection) {
-      case PlayerDirection.left:
-        if (isfacingright){
-          flipHorizontallyAroundCenter();
-          isfacingright= false;
-        }
-        current= PlayerState.running;
-        dirX-=moveSpeed;
-        break;
-      case PlayerDirection.right:
-        if (!isfacingright){
-          flipHorizontallyAroundCenter();
-          isfacingright= true ;
-        }
-        current= PlayerState.running;
-        dirX+=moveSpeed;
-        break;
-      case PlayerDirection.none:
-        current= PlayerState.idle; 
-        break;
-      default: 
-    }
-    velocity= Vector2(dirX, 0.0);
-    position += velocity * dt;
+    
+    //velocity= Vector2(dirX, 0.0);
+    position.x += velocity.x * dt;
   }
 }
